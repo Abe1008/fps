9d9e0b210c3ef22c6d26678514f6434733da8324
ae711e <ae711e@gmail.com>
2017-02-07 17:35:19 +0300

    добавил класс формирования Excel

---
+++ src/ae/ExcelList.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017. Aleksey Eremin
+ * 07.02.17 17:28
+ */
+
+package ae;
+
+/**
+ * Created by ae on 07.02.2017.
+ * заполняет данными лист Excel для одного месяца
+ */
+public class ExcelList {
+    
+    public ExcelList()
+    {
+        
+    }
+    
+    /**
+     * Записывает в выходной файл данные из БД для указанного месяца
+     * @param db    база данных
+     * @param year  год
+     * @param month месяц
+     * @return      кол-во записей для указанного месяца
+     */
+    public int writeList(Database db, int year, int month)
+    {
+        return 0;
+    }
+    
+}
